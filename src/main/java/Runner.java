fdc6fcb3a5df97a265a8bc6d8b911da3e99487a8
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-19 20:30:45 +0100

    Adding Runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,8 @@
 import db.DBHelper;
 import models.character.Character;
+import models.creatures.Pet;
+import models.interfaces.IPlayerClass;
+import models.items.Potion;
 import models.playerclasses.Barbarian;
 import models.playerraces.Dwarf;
 
@@ -11,5 +14,16 @@ public class Runner {
 		Barbarian barbarian = new Barbarian();
 		Character vicky = new Character("Vicky", dwarf, barbarian);
 		DBHelper.save(vicky);
+
+		IPlayerClass vickysRace = vicky.getPlayerClass();
+		vicky.addPotion(new Potion());
+		vicky.addPet(new Pet());
+		DBHelper.update(vicky);
+
+
+
 	}
+
+
+
 }
